b746defdf0c1ac4ff5e73cb9bc892ef8f38e378d
tangrowth <to.chinatsu@icloud.com>
2022-04-10 19:12:03 +0900

    4/10

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -43,6 +43,11 @@ class PostController extends Controller
         $post->delete();
         return redirect('/');
     }
+    
+    public function create(Category $category)
+    {
+        return view('posts/create')->with(['categories' => $category->get()]);;
+    }
 }
 
 ?>
